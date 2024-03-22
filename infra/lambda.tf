9af0cd362422daa71f34cc274070efea1152a39c
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-03-21 21:20:35 -0300

    fix: depends on

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,4 +1,6 @@
 resource "aws_lambda_function" "lambda_function" {
+  depends_on = [ aws_s3_object.handler_file ]
+
   function_name = "binance_trades"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "src.handler.handler"
