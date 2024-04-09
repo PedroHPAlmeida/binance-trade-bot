3ed89190f572116d25b72713bf9b066566a136cc
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-04-08 22:13:42 -0300

    ci: fix bug

--- src/handler.py
+++ src/handler.py
@@ -1,27 +1,27 @@
-from .binance import Binance
-from .mongo import Mongo
-from .price_now_repo import PriceNowRepository
-from .statistics_24hr_repo import Statistics24hRepository
+# from .binance import Binance
+# from .mongo import Mongo
+# from .price_now_repo import PriceNowRepository
+# from .statistics_24hr_repo import Statistics24hRepository
 
-binance = Binance()
+# binance = Binance()
 
 
 def handler(event, context):
     try:
-        save_trades_24h()
-        save_prices_now()
+        # save_trades_24h()
+        # save_prices_now()
         return {'status': 'success'}
     except Exception as ex:
         return {'status': 'error', 'message': str(ex)}
 
 
-def save_trades_24h():
-    statistics_24h_repo = Statistics24hRepository(Mongo('trades', 'statistics_24hr_by_usdt'))
-    trades_24h = binance.statistics_24hr_by_usdt()
-    statistics_24h_repo.save(trades_24h)
+# def save_trades_24h():
+#     statistics_24h_repo = Statistics24hRepository(Mongo('trades', 'statistics_24hr_by_usdt'))
+#     trades_24h = binance.statistics_24hr_by_usdt()
+#     statistics_24h_repo.save(trades_24h)
 
 
-def save_prices_now():
-    price_now_repo = PriceNowRepository(Mongo('trades', 'price_now_by_usdt'))
-    prices_now = binance.price_now_by_usdt()
-    price_now_repo.save(prices_now)
+# def save_prices_now():
+#     price_now_repo = PriceNowRepository(Mongo('trades', 'price_now_by_usdt'))
+#     prices_now = binance.price_now_by_usdt()
+#     price_now_repo.save(prices_now)
