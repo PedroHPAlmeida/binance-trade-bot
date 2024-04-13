32f3aa6833c15032e48230e35fc1aac86ee86697
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-04-13 13:30:09 -0300

    fix: corrige prompt

--- handler.py
+++ handler.py
@@ -9,7 +9,7 @@ PROMPTS = {
 I want you to write a creative message stating that the script ran successfully. \
 As the message will be sent via Telegram, avoid characters that will not be interpreted correctly. \
 Use emojis as much as you like. The structure of the message must consist of a title, body and conclusion. \
-However, don't put the words 'Title', 'Message:' and 'Conclusion:' explicitly, just follow the structure. \
+However, don't put the words 'Title:', 'Message:' and 'Conclusion:' to indicate the section, just follow the structure.. \
 Keep an average of approximately 400 characters in your response. \
 I will also give you the script execution timestamp, include it somewhere in the message. \
 The timestamp is UTC 0, convert it to UTC +3 and format the date and time in the following pattern: dd/MM/YYYY hh:mm:ss. \
@@ -18,7 +18,7 @@ Below is the execution time of the script ${execution_time}. The message must be
 I want you to write a creative message stating that an error occurred while running the script. \
 As the message will be sent via Telegram, avoid characters that will not be interpreted correctly. Use emojis as much as you want. \
 The structure of the message must consist of title, body and conclusion. \
-However, don't put the words 'Title', 'Message:' and 'Conclusion:' explicitly, just follow the structure. \
+However, don't put the words 'Title:', 'Message:' and 'Conclusion:' to indicate the section, just follow the structure.. \
 Keep your response to an average of approximately 400 characters. \
 I will also give you the script execution timestamp by including it somewhere in the message. \
 The timestamp is UTC 0, convert it to UTC +3 and format the date and time in the following pattern: dd/MM/YYYY hh:mm:ss. \
@@ -55,7 +55,7 @@ def define_message(status: str, **kwargs) -> str:
     message = PROMPTS[status]
     kwargs['execution_time'] = datetime.now().isoformat()
     for key, value in kwargs.items():
-        message = message.replace(f'${key}', value)
+        message = message.replace('${' + key + '}', value)
     return ai.generate(message)
 
 
