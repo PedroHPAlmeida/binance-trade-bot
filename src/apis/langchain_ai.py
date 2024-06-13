4621726f4dfd576d50892500b295870d7b6331d8
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-06-13 19:47:09 -0300

    fix: corrige resposta

--- src/apis/langchain_ai.py
+++ src/apis/langchain_ai.py
@@ -1,14 +1,17 @@
 import os
 
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 
 
 class LLM:
     def __init__(self, token: str = os.getenv('OPENAI_API_KEY')) -> None:
         self._llm = ChatOpenAI(api_key=token)
+        self._str_parser = StrOutputParser()
 
     def generate(self, prompt: str | ChatPromptTemplate, **kwargs) -> str:
         if isinstance(prompt, ChatPromptTemplate):
             prompt = prompt.format_messages(**kwargs)
-        return self._llm.invoke(prompt)['content']
+        chain = self._llm | self._str_parser
+        return chain.invoke(prompt)
